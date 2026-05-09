da4e67210f2b3abb5565fec89811f397e87952e1
sardus <sardus@yandex.ru>
2026-05-09 23:29:27 +0300

    Очередной коммит

--- Lesson_004/src/main/java/org/example/User.java
+++ Lesson_004/src/main/java/org/example/User.java
@@ -1,30 +1,9 @@
 package org.example;
 
-public class User {
-    private final String name;
-    private final int age;
-    private final String email;
-
-    public User(String name, int age, String email) {
-        this.name = name;
-        this.age = age;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
+public record User(String name, int age, String email) {
 
     @Override
-    public String toString(){
-        return "User{Your name='" + getName() + "', Your age=" + getAge() + ", Your email='" + getEmail() + "'}";
+    public String toString() {
+        return "User{Your name='" + name() + "', Your age=" + age() + ", Your email='" + email() + "'}";
     }
 }
